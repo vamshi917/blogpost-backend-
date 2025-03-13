aa2103d058cd3679cbec4430a09a8f4274c3af59
manvikadornala <vamshi.eppakayala917@gmail.com>
2025-03-13 20:49:11 +0530

    added/routes

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(cors());
 connectDb();
 
 //routes
+app.get('/', (req, res) => {
+  res.send('Hello from Express API deployed on Vercel!');
+});
+
 app.post("/post-blog", async (req, res) => {
   let blog = new BlogPost({
     title: req.body.title,
@@ -24,6 +28,8 @@ app.post("/post-blog", async (req, res) => {
   await blog.save();
   res.status(200).json({ message: "post saved", blog });
 });
+
+
 
 app.get("/get-blogs", async (req, res) => {
   let blogs = await BlogPost.find();
@@ -44,19 +50,25 @@ app.delete("/delete-blog/:id", async (req, res) => {
 //  n
 
 app.put("/update-blog/:id", async (req, res) => {
-  const { title, description } = req.body;
+  // const { title, description } = req.body;
   let blog = await BlogPost.findByIdAndUpdate(req.params.id);
 
-  if (!title && !description) {
-    res.status(404).json({ message: "No blog found..." });
-  } else if (!title) {
-    blog.description = description;
-  } else if (!description) {
-    blog.title = title;
-  } else {
-    blog.title = title;
-    blog.description = description;
+  if(!blog){
+    res.status(404).json({message : "No blog found"})
   }
+
+  if (!req.body.title && !req.body.description) {
+    res.json({ message: "Please enter a title and description..." });
+  } else if (!req.body.title) {
+    blog.description = req.body.description;
+  } else if (!req.body.description) {
+    blog.title = req.body.title;
+  } else {
+    blog.title = req.body.title;
+    blog.description = req.body.description;
+  }
+  await blog.save();
+  res.status(200).json({message : "Blog updated successfully.."})
 });
 
 // listen server
